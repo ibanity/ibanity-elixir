6d914c5a408f91d7bdcb55a4570e8163efb33c71
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-23 17:06:24 +0200

    Set up tests

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule IbanityElixir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      preferred_cli_env: [espec: :test],
       deps: deps()
     ]
   end
@@ -21,6 +22,7 @@ defmodule IbanityElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:espec, "~> 1.6.2", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
