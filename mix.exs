af3bfa5aa241607d80081b89c22c6e6a6fd755ea
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-25 14:20:57 +0200

    Add Jason as dependency

--- mix.exs
+++ mix.exs
@@ -24,9 +24,8 @@ defmodule Ibanity.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.4"},
+      {:jason, "~> 1.1"},
       {:espec, "~> 1.6.2", only: :test}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
