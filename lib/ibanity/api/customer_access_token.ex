762777656a21c65c036b99a9d7283add6d521a14
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-26 22:43:08 +0200

    Customer access token creation

---
+++ lib/ibanity/api/customer_access_token.ex
@@ -0,0 +1,23 @@
+defmodule Ibanity.CustomerAccessToken do
+  alias Ibanity.{Client, Collection, CustomerAccessToken, Request, ResourceOperations}
+
+  @base_keys [:token]
+  @enforce_keys [:id | @base_keys]
+  defstruct id: nil, token: nil
+
+  def create(application_customer_reference, idempotency_key \\ nil) do
+    uri = Map.get(Client.api_schema(), "customerAccessTokens")
+
+    request =
+      uri
+      |> Request.new
+      |> Request.resource_type("customerAccessToken")
+      |> Request.idempotency_key(idempotency_key)
+      |> Request.attributes(%{application_customer_reference: application_customer_reference})
+      |> Request.build
+
+    ResourceOperations.create_by_uri(__MODULE__, request)
+  end
+
+  def keys, do: @base_keys
+end
