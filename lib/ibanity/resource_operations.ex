29fb6f9a684f7e3ccb1edcf7b0fd16b37e7e5a83
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-26 22:42:44 +0200

    Wrap single resource

--- lib/ibanity/resource_operations.ex
+++ lib/ibanity/resource_operations.ex
@@ -2,7 +2,10 @@ defmodule Ibanity.ResourceOperations do
   alias Ibanity.{BaseResource, Client, Collection, Request}
 
   def create_by_uri(module, %Request{} = request) do
-    raw_response = Client.post(request)
+    request
+    |> Client.post
+    |> Map.fetch!("data")
+    |> wrap_item(module)
   end
 
   def list_by_uri(module, %Request{} = request) do
@@ -20,4 +23,7 @@ defmodule Ibanity.ResourceOperations do
     Enum.map(data, &(BaseResource.new(module, &1)))
   end
 
+  defp wrap_item(data, module) do
+    BaseResource.new(module, data)
+  end
 end
