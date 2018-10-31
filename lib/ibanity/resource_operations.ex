81558cfeb73a9cddd639c9ed7550175f6aa35d05
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-10-31 10:44:34 +0100

    Improve response handling, especially for errors

--- lib/ibanity/resource_operations.ex
+++ lib/ibanity/resource_operations.ex
@@ -1,7 +1,7 @@
 defmodule Ibanity.ResourceOperations do
   alias Ibanity.{BaseResource, Client, Client.Request, Collection}
 
-  def create(%Request{} = request, return_type) do
+  def create(%Request{} = request, return_type \\ nil) do
     execute_request(:post, request, return_type)
   end
 
@@ -27,13 +27,14 @@ defmodule Ibanity.ResourceOperations do
     |> handle_response(return_type)
   end
 
+  defp handle_response({:error, _} = error, _return_type), do: error
+  defp handle_response({:ok, res}, nil), do: res
   defp handle_response({:ok, items}, return_type) when is_list(items) do
     items
     |> wrap(return_type)
     |> Collection.new(%{}, %{}, return_type)
   end
   defp handle_response({:ok, item}, return_type), do: wrap(item, return_type)
-  defp handle_response(error, _return_type), do: error
 
   defp wrap(data, return_type) when is_list(data) do
     Enum.map(data, &wrap(&1, return_type))
