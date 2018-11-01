4309172fa92f214240b35356f97ba728516a4944
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-11-01 18:20:13 +0100

    Remove unused dependencies

--- mix.exs
+++ mix.exs
@@ -25,10 +25,7 @@ defmodule Ibanity.MixProject do
     [
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1"},
-      {:recase, "~> 0.3"},
-      {:elixir_uuid, "~> 1.2"},
-      {:espec, "~> 1.6.2", only: :test},
-      {:exvcr, "~> 0.10.3", only: :test}
+      {:recase, "~> 0.3"}
     ]
   end
 end
