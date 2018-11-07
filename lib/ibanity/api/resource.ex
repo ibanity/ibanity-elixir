d5cca830450539513d00ceea8328fd5ad98ae0b5
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-11-07 14:03:57 +0100

    Please Credo

--- lib/ibanity/api/resource.ex
+++ lib/ibanity/api/resource.ex
@@ -1,4 +1,7 @@
 defmodule Ibanity.Resource do
+  @moduledoc """
+  Common properties for resources
+  """
   defmacro __using__(_ \\ []) do
     quote do
       alias Ibanity.Client.Request, as: ClientRequest
