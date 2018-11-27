6e49b90d3ebb4e90a829135e471224874b2d6763
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-11-27 11:21:12 +0100

    Minimal configuration for test environment

--- config/test.exs
+++ config/test.exs
@@ -1,2 +1,8 @@
 use Mix.Config
 
+config :ibanity, :applications, [
+  default: []
+]
+
+config :ibanity, :api_url, System.get_env("IBANITY_API_URL")
+config :ibanity, :ssl_ca_file, System.get_env("IBANITY_CA_FILE")
