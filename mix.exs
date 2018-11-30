b6def12eb2fa8d3fce5049f63b258eeb062135bf
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2018-11-30 10:30:28 +0100

    Release 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ibanity.MixProject do
   def project do
     [
       app: :ibanity,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: "Wrapper for the Ibanity API",
@@ -33,7 +33,7 @@ defmodule Ibanity.MixProject do
       {:recase, "~> 0.3"},
       {:ex_crypto, "~> 0.9.0"},
       {:ex_doc, "~> 0.18.0", only: :dev, runtime: false},
-      {:credo, "~> 1.0.0-rc1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
