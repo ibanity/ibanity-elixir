c70e9f9d0b42c9a9e31ad57c02f7c5e996397014
Christophe Leclef <christophe.leclef@ibanity.com>
2020-06-17 13:43:04 +0200

    add new attributes to spec

--- test/lib/deserialization/sandbox/financial_institution_transaction_test.exs
+++ test/lib/deserialization/sandbox/financial_institution_transaction_test.exs
@@ -34,7 +34,16 @@ defmodule Ibanity.Sandbox.FinancialInstitutionTransaction.DeserializationTest do
         "createdAt" => "2018-10-18T15:13:12.365318Z",
         "counterpartReference" => "BE9786154282554",
         "counterpartName" => "Otro Bank",
-        "amount" => 84.42
+        "amount" => 84.42,
+        "internalReference" => "ref_123",
+        "bankTransactionCode" => "PMNT-IRCT-ESCT",
+        "proprietaryBankTransactionCode "=> "prop123",
+        "endToEndId" => "61dd468606594217af9965ad3928280d",
+        "purposeCode" => "CASH",
+        "mandateId" => "12345678",
+        "creditorId" => "98765",
+        "digest" => "d59f256988324499809c18a8d4a8be60ff36e3d1e0c89c380be49e032e39a287",
+        "additionalInformation" => "addional"
       }
     }
 
@@ -53,6 +62,15 @@ defmodule Ibanity.Sandbox.FinancialInstitutionTransaction.DeserializationTest do
       counterpart_reference: "BE9786154282554",
       counterpart_name: "Otro Bank",
       amount: 84.42,
+      internal_reference: "ref_123",
+      bank_transaction_code: "PMNT-IRCT-ESCT",
+      proprietary_bank_transaction_code: "prop123",
+      end_to_end_id: "61dd468606594217af9965ad3928280d",
+      purpose_code: "CASH",
+      mandate_id: "12345678",
+      creditor_id:  "98765",
+      digest: "d59f256988324499809c18a8d4a8be60ff36e3d1e0c89c380be49e032e39a287",
+      additional_information: "addional",
       financial_institution_account_id: "d9d60751-b741-4fa6-8524-8f9a066ca037"
     }
 
