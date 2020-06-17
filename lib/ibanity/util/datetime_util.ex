05598903d951436e70550ae9fcc357a8edfebb05
Bruno Dusausoy <bruno.dusausoy@ibanity.com>
2020-06-17 17:28:05 +0200

    Raise an error if datetime field cannot be parsed instead of silently returning nil

--- lib/ibanity/util/datetime_util.ex
+++ lib/ibanity/util/datetime_util.ex
@@ -3,8 +3,11 @@ defmodule Ibanity.DateTimeUtil do
 
   def parse(string) do
     case DateTime.from_iso8601(string) do
-      {:ok, datetime, 0} -> datetime
-      {:error, _} -> nil
+      {:ok, datetime, 0} ->
+        datetime
+
+      {:error, _} ->
+        raise ArgumentError, message: "cannot parse #{string} as datetime"
     end
   end
 end
