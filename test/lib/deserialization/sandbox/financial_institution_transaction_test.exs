467841e141203da239866536e7d3ff1c439c4b9a
Christophe Leclef <christophe.leclef@ibanity.com>
2020-06-17 13:49:04 +0200

    typo in spec

--- test/lib/deserialization/sandbox/financial_institution_transaction_test.exs
+++ test/lib/deserialization/sandbox/financial_institution_transaction_test.exs
@@ -37,7 +37,7 @@ defmodule Ibanity.Sandbox.FinancialInstitutionTransaction.DeserializationTest do
         "amount" => 84.42,
         "internalReference" => "ref_123",
         "bankTransactionCode" => "PMNT-IRCT-ESCT",
-        "proprietaryBankTransactionCode "=> "prop123",
+        "proprietaryBankTransactionCode" => "prop123",
         "endToEndId" => "61dd468606594217af9965ad3928280d",
         "purposeCode" => "CASH",
         "mandateId" => "12345678",
