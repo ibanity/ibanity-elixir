0cc541805aeebec571cb9336cab38ba47bfd34c2
Bram Jans <ik@bramjans.com>
2020-08-03 17:05:57 +0200

    Add attributes to financial institution deserialization test

--- test/lib/deserialization/xs2a/financial_institution_test.exs
+++ test/lib/deserialization/xs2a/financial_institution_test.exs
@@ -12,7 +12,12 @@ defmodule Ibanity.Xs2a.FinancialInstitution.DeserializationTest do
       "id" => "01faf09c-038d-43f3-8e0c-d0aaf3e0e176",
       "attributes" => %{
         "sandbox" => true,
-        "name" => "ABN AMRO HOARE GOVETT CORPORATE FINANCE LTD. 7"
+        "name" => "ABN AMRO HOARE GOVETT CORPORATE FINANCE LTD. 7",
+        "maxRequestedAccountReferences" => 3,
+        "minRequestedAccountReferences" => 1,
+        "requiresCredentialStorage" => false,
+        "paymentsProductTypes" => ["sepaCreditTransfer"],
+        "authorizationModels" => ["detailed", "financialInstitutionOffered"]
       }
     }
 
@@ -22,6 +27,11 @@ defmodule Ibanity.Xs2a.FinancialInstitution.DeserializationTest do
       id: "01faf09c-038d-43f3-8e0c-d0aaf3e0e176",
       sandbox: true,
       name: "ABN AMRO HOARE GOVETT CORPORATE FINANCE LTD. 7",
+      max_requested_account_references: 3,
+      min_requested_account_references: 1,
+      requires_credential_storage: false,
+      payments_product_types: ["sepaCreditTransfer"],
+      authorization_models: ["detailed", "financialInstitutionOffered"],
       self_link:
         "https://api.ibanity.com/financial-institutions/01faf09c-038d-43f3-8e0c-d0aaf3e0e176"
     }
