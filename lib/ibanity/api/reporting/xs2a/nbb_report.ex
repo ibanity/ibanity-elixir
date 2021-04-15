f82493a9d87d3304eae796f0c71dd1bdcf9f72a7
Loic Vigneron <loic@ibanity.com>
2021-04-15 16:02:56 +0200

    Fix nbb reporting schema, use string instead of integer and remove useless field

--- lib/ibanity/api/reporting/xs2a/nbb_report.ex
+++ lib/ibanity/api/reporting/xs2a/nbb_report.ex
@@ -7,7 +7,6 @@ defmodule Ibanity.Reporting.Xs2a.NbbReport do
             end: nil,
             account_information_accounts: nil,
             account_information_synchronization_count: nil,
-            account_information_synchronizations: nil,
             payment_initiation_accounts: nil,
             payment_initiations: nil
 
@@ -21,8 +20,7 @@ defmodule Ibanity.Reporting.Xs2a.NbbReport do
       beginning: {~w(attributes beginning), :datetime},
       end: {~w(attributes end), :datetime},
       account_information_accounts: {~w(attributes accountInformationAccounts), :string},
-      account_information_synchronization_count: {~w(attributes accountInformationSynchronizationCount), :integer},
-      account_information_synchronizations: {~w(attributes accountInformationSynchronizations), :string},
+      account_information_synchronization_count: {~w(attributes accountInformationSynchronizationCount), :string},
       payment_initiation_accounts: {~w(attributes paymentInitiationAccounts), :string},
       payment_initiations: {~w(attributes paymentInitiations), :string}
     ]
