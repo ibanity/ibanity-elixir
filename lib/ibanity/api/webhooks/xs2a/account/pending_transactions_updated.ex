6f07cbb7f4e3475cd03710ee30f3cdebf0986a78
Christophe Leclef <christophe.leclef@ibanity.com>
2023-04-04 10:43:05 +0200

    Add webhook reference

---
+++ lib/ibanity/api/webhooks/xs2a/account/pending_transactions_updated.ex
@@ -0,0 +1,23 @@
+defmodule Ibanity.Webhooks.Xs2a.Account.PendingTransactionsUpdated do
+  @moduledoc """
+  xs2a.account.pendingTransactionsUpdated webhook event
+  """
+
+  defstruct id: nil,
+            created_at: nil,
+            count: nil,
+            account_id: nil,
+            synchronization_id: nil,
+            batch_synchronization_id: nil
+
+  def key_mapping do
+    [
+      id: {~w(id), :string},
+      created_at: {~w(attributes createdAt), :datetime},
+      count: {~w(attributes count), :integer},
+      account_id: {~w(relationships account data id), :string},
+      synchronization_id: {~w(relationships synchronization data id), :string},
+      batch_synchronization_id: {~w(relationships batchSynchronization data id), :string}
+    ]
+  end
+end
