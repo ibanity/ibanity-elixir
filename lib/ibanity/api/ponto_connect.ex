3476a819aacc8dff7128fc8221d846573806fe68
Fabian Zitter <fzitter@isabelgroup.eu>
2024-09-13 10:45:15 +0200

    Add docs and tests for PontoConnect module

--- lib/ibanity/api/ponto_connect.ex
+++ lib/ibanity/api/ponto_connect.ex
@@ -7,12 +7,76 @@ defmodule Ibanity.PontoConnect do
 
   @doc """
   Adds default ponto connect attributes needed for a token request into attributes using the
-  configured values, and sets the `%Request{}.customer_access_token` required for the authorization header.
+  configured values, and sets the required headers.
+
+  This function needs the Ponto Connect configuration to be set correctly for the application used:
+
+      config :ibanity,
+        api_url: System.get_env("IBANITY_API_URL"),
+        applications: [
+          default: [
+            ...
+            ponto_connect_client_id: System.get_env("PONTO_CONNECT_CLIENT_ID"),
+            ponto_connect_client_secret: System.get_env("PONTO_CONNECT_CLIENT_SECRET")
+          ]
+        ]
+
+  and the `%Request{}` struct has to be set up with the correct attributes for the intended token.
 
   ## Examples
 
-      iex> PontoConnect.token_default_request(%Request{})
-      %Request{attributes: %{grant_type: "authorization_code", client_id: "542cbc7a-7968-426c-830f-f9dc1c3c358a"}}
+  ### Client token
+
+      iex> PontoConnect.create_token_default_request(%Request{})
+      %Request{
+        attributes: %{
+          grant_type: "client_credentials",
+          client_id: "542cbc7a-7968-426c-830f-f9dc1c3c358a"
+        },
+        headers: [
+          {:Authorization, "Basic NTQyY2JjN2EtNzk2OC00MjZjLTgzMGYtZjlkYzFjM2MzNThhOnRlc3QtY2xpZW50LXNlY3JldA=="},
+          {:Accept, "application/json"},
+          {:"Content-Type", "application/json"}
+        ]
+      }
+
+  ### Refresh token
+
+      iex> [refresh_token: "example-token"]
+      ...> |> Request.attributes()
+      ...> |> PontoConnect.create_token_default_request()
+      %Request{
+        attributes: %{
+          grant_type: "refresh_token", 
+          client_id: "542cbc7a-7968-426c-830f-f9dc1c3c358a",
+          refresh_token: "example-token"
+        },
+        headers: [
+          {:Authorization, "Basic NTQyY2JjN2EtNzk2OC00MjZjLTgzMGYtZjlkYzFjM2MzNThhOnRlc3QtY2xpZW50LXNlY3JldA=="},
+          {:Accept, "application/json"},
+          {:"Content-Type", "application/json"}
+        ]
+      }
+
+  ### Initial token
+
+      iex> [code: "example-code", redirect_uri: "https://example.com/oaut2/return", code_verifier: "pkce-code-verifier"]
+      ...> |> Request.attributes()
+      ...> |> PontoConnect.create_token_default_request()
+      %Request{
+        attributes: %{
+          grant_type: "authorization_code",
+          client_id: "542cbc7a-7968-426c-830f-f9dc1c3c358a",
+          code: "example-code",
+          redirect_uri: "https://example.com/oaut2/return",
+          code_verifier: "pkce-code-verifier"
+        },
+        headers: [
+          {:Authorization, "Basic NTQyY2JjN2EtNzk2OC00MjZjLTgzMGYtZjlkYzFjM2MzNThhOnRlc3QtY2xpZW50LXNlY3JldA=="},
+          {:Accept, "application/json"},
+          {:"Content-Type", "application/json"}
+        ]
+      }
   """
   def create_token_default_request(%Request{} = request) do
     new_attributes =
@@ -39,6 +103,36 @@ defmodule Ibanity.PontoConnect do
     end
   end
 
+  @doc """
+  Adds default ponto connect attributes needed for a revoke token request into attributes using the
+  configured values, and sets the required headers.
+
+  This function needs the Ponto Connect configuration to be set correctly for the application used:
+
+  config :ibanity,
+  api_url: System.get_env("IBANITY_API_URL"),
+  applications: [
+    default: [
+      ...
+      ponto_connect_client_id: System.get_env("PONTO_CONNECT_CLIENT_ID"),
+      ponto_connect_client_secret: System.get_env("PONTO_CONNECT_CLIENT_SECRET")
+    ]
+  ]
+
+  ## Examples
+
+  iex> PontoConnect.delete_token_default_request(%Request{})
+  %Request{
+    attributes: %{
+      client_id: "542cbc7a-7968-426c-830f-f9dc1c3c358a"
+    },
+    headers: [
+      {:Authorization, "Basic NTQyY2JjN2EtNzk2OC00MjZjLTgzMGYtZjlkYzFjM2MzNThhOnRlc3QtY2xpZW50LXNlY3JldA=="},
+      {:Accept, "application/json"},
+      {:"Content-Type", "application/json"}
+    ]
+  }
+  """
   def delete_token_default_request(%Request{} = request) do
     new_attributes =
       %{
