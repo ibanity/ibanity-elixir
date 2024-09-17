3cd39810ff49954c0ea958be011cc9d27dde0094
Fabian Zitter <fzitter@isabelgroup.eu>
2024-09-17 14:39:19 +0200

    Add Sandbox resources

---
+++ lib/ibanity/api/ponto_connect/sandbox/financial_institution_transaction.ex
@@ -0,0 +1,217 @@
+defmodule Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction do
+  @moduledoc """
+  [Sandbox Financial Institution Transaction](https://documentation.ibanity.com/ponto-connect/api#financial-institution-transaction) API wrapper
+  """
+
+  use Ibanity.Resource
+
+  @api_schema_path [
+    "ponto-connect",
+    "sandbox",
+    "financialInstitution",
+    "financialInstitutionAccount",
+    "financialInstitutionTransactions"
+  ]
+
+  defstruct [
+    :id,
+    :description,
+    :currency,
+    :created_at,
+    :updated_at,
+    :amount,
+    :fee,
+    :additional_information,
+    :bank_transaction_code,
+    :card_reference,
+    :card_reference_type,
+    :counterpart_name,
+    :counterpart_reference,
+    :creditor_id,
+    :end_to_end_id,
+    :execution_date,
+    :mandate_id,
+    :proprietary_bank_transaction_code,
+    :purpose_code,
+    :remittance_information,
+    :remittance_information_type,
+    :value_date
+  ]
+
+  alias Ibanity.PontoConnect
+
+  @doc """
+  [List sandbox Financial Institution Transactions](https://documentation.ibanity.com/ponto-connect/2/api#list-financial-institution-transactions)
+
+  Takes a `Ibanity.PontoConnect.Token`, or a `Ibanity.Request` with set `:customer_access_token` as argument.
+
+  #{PontoConnect.common_docs!(:financial_institution_and_account_second_arg)}
+
+  ## Examples
+
+  #{PontoConnect.common_docs!(:financial_institution_and_account_ids)}
+
+  Use `financial_institution_or_id` and `financial_institution_account_or_id` to list Financial Institution Transactions
+
+      iex> token |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.list(%{
+      ...>   financial_institution_id: financial_institution_or_id,
+      ...>   financial_institution_account_id: financial_institution_account_or_id
+      ...> })
+      {:ok, %Ibanity.Collection{
+        items: [%Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction{}]
+      }}
+
+      iex> token |> Ibanity.Request.customer_access_token() |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.list(financial_institution_or_id)
+      {:ok, %Ibanity.Collection{
+        items: [%Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction{}]
+      }}
+
+      iex> invalid_token |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.list(financial_institution_or_id)
+      {:error,
+        [
+          %{
+            "code" => "invalidAccessToken",
+            "detail" => "Your access token is invalid.",
+            "meta" => %{"requestId" => "00077F000001D3A87F0000011F4066E43AFD1900051"}
+          }
+        ]}
+
+  """
+  def list(
+        %Request{customer_access_token: customer_access_token} = request_or_token,
+        %{
+          financial_institution_id: financial_institution_or_id,
+          financial_institution_account_id: financial_institution_account_or_id
+        } = ids
+      )
+      when not is_nil(customer_access_token) and not is_nil(financial_institution_or_id) and
+             not is_nil(financial_institution_account_or_id) do
+    formatted_ids =
+      ids
+      |> Map.put_new(:id, "")
+      |> PontoConnect.format_ids()
+
+    request_or_token
+    |> Request.ids(formatted_ids)
+    |> Client.execute(:get, @api_schema_path, __MODULE__)
+  end
+
+  def list(%PontoConnect.Token{} = token, ids) do
+    token
+    |> Request.customer_access_token()
+    |> list(ids)
+  end
+
+  def list(other, _ids) do
+    raise ArgumentError,
+      message: PontoConnect.token_argument_error_msg("FinancialInstitutionTransaction", other)
+  end
+
+  @doc """
+  [Find sandbox Financial Institution Transaction by id](https://documentation.ibanity.com/ponto-connect/2/api#get-financial-institution-transaction)
+
+  Takes a `Ibanity.PontoConnect.Token`, or a `Ibanity.Request` with set `:customer_access_token` as first argument.
+
+  #{PontoConnect.common_docs!(:financial_institution_and_account_second_arg)}
+
+  ## Examples
+
+  #{PontoConnect.common_docs!(:financial_institution_and_account_ids)}
+
+  Use `financial_institution_or_id` and `financial_institution_account_or_id` to find a Financial Institution Transactions
+
+      iex> token
+      ...> |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.find(%{
+      ...>   financial_institution_id: financial_institution_or_id,
+      ...>   financial_institution_account_id: financial_institution_account_or_id,
+      ...>   id: "953934eb-229a-4fd2-8675-07794078cc7d"
+      ...> })
+      {:ok, %Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction{id: "953934eb-229a-4fd2-8675-07794078cc7d"}}
+
+      iex> token
+      ...> |> Ibanity.Request.customer_access_token()
+      ...> |> Ibanity.Request.application(:my_application)
+      ...> |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.find(%{
+      ...>   financial_institution_id: financial_institution_or_id,
+      ...>   financial_institution_account_id: financial_institution_account_or_id,
+      ...>   id: "953934eb-229a-4fd2-8675-07794078cc7d"
+      ...> })
+      {:ok, %Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction{id: "953934eb-229a-4fd2-8675-07794078cc7d"}}
+
+      iex> token
+      ...> |> Ibanity.PontoConnect.Sandbox.FinancialInstitutionTransaction.find(%{
+      ...>   financial_institution_id: financial_institution_or_id,
+      ...>   financial_institution_account_id: financial_institution_account_or_id,
+      ...>   id: "does-not-exist"
+      ...> })
+      {:error,
+        [
+          %{
+            "code" => "resourceNotFound",
+            "detail" => "The requested resource was not found.",
+            "meta" => %{
+              "requestId" => "00077F00000184847F0000011F4066E44223327005A",
+              "resource" => "Sandbox.FinancialInstitutionTransaction"
+            }
+          }
+        ]}
+  """
+  def find(
+        %Request{customer_access_token: token} = request_or_token,
+        %{
+          financial_institution_id: financial_institution_or_id,
+          financial_institution_account_id: financial_institution_account_or_id,
+          id: id
+        } = ids
+      )
+      when not is_nil(token) and not is_nil(financial_institution_or_id) and not is_nil(id) and
+             not is_nil(financial_institution_account_or_id) do
+    formatted_ids = PontoConnect.format_ids(ids)
+
+    request_or_token
+    |> Request.ids(formatted_ids)
+    |> Client.execute(:get, @api_schema_path, __MODULE__)
+  end
+
+  def find(%PontoConnect.Token{} = request_or_token, ids) do
+    request_or_token
+    |> Request.customer_access_token()
+    |> find(ids)
+  end
+
+  def find(other, _id) do
+    raise ArgumentError,
+      message: PontoConnect.token_argument_error_msg("FinancialInstitutionTransaction", other)
+  end
+
+  alias Ibanity.PontoConnect
+
+  @doc false
+  def key_mapping do
+    [
+      id: {["id"], :string},
+      description: {["attributes", "description"], :string},
+      currency: {["attributes", "currency"], :string},
+      created_at: {["attributes", "createdAt"], :datetime},
+      updated_at: {["attributes", "updatedAt"], :datetime},
+      amount: {["attributes", "amount"], :number},
+      fee: {["attributes", "fee"], :number},
+      additional_information: {["attributes", "additionalInformation"], :string},
+      bank_transaction_code: {["attributes", "bankTransactionCode"], :string},
+      card_reference: {["attributes", "cardReference"], :string},
+      card_reference_type: {["attributes", "cardReferenceType"], :string},
+      counterpart_name: {["attributes", "counterpartName"], :string},
+      counterpart_reference: {["attributes", "counterpartReference"], :string},
+      creditor_id: {["attributes", "creditorId"], :string},
+      end_to_end_id: {["attributes", "endToEndId"], :string},
+      execution_date: {["attributes", "executionDate"], :datetime},
+      mandate_id: {["attributes", "mandateId"], :string},
+      proprietary_bank_transaction_code:
+        {["attributes", "proprietaryBankTransactionCode"], :string},
+      purpose_code: {["attributes", "purposeCode"], :string},
+      remittance_information: {["attributes", "remittanceInformation"], :string},
+      remittance_information_type: {["attributes", "remittanceInformationType"], :string},
+      value_date: {["attributes", "valueDate"], :datetime}
+    ]
+  end
+end
