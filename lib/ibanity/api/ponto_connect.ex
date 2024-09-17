20b547668da6e31a2cb86993d17abc025e0c170c
Fabian Zitter <fzitter@isabelgroup.eu>
2024-09-17 14:37:51 +0200

    Update common docs

--- lib/ibanity/api/ponto_connect.ex
+++ lib/ibanity/api/ponto_connect.ex
@@ -177,30 +177,66 @@ defmodule Ibanity.PontoConnect do
     Fetch an account before each example, or use a valid account id
 
         iex> {:ok, account_or_id} = token |> Ibanity.PontoConnect.Account.find("03ebe0ae-f630-4414-b37b-afde7de67229")
-        {:ok,
-          %Ibanity.PontoConnect.Account{
-          id: "03ebe0ae-f630-4414-b37b-afde7de67229"
-        }}
+
+    Or
 
         iex> account_or_id = "03ebe0ae-f630-4414-b37b-afde7de67229"
         "03ebe0ae-f630-4414-b37b-afde7de67229"
     """,
-    account_and_id_second_arg: """
-    Takes a map with the following keys as second argument:
-    - `:account_id``Ibanity.PontoConnect.Account` struct or account ID as a string
-    - `:id` resource ID as a string
-    """,
     synchronization_id: """
-    Fetch an synchronization before each example, or use a valid synchronization id
+    Fetch a synchronization before each example, or use a valid synchronization id
 
         iex> {:ok, synchronization_or_id} = token |> Ibanity.PontoConnect.Suynchronization.find("03ebe0ae-f630-4414-b37b-afde7de67229")
-        {:ok,
-          %Ibanity.PontoConnect.Account{
-          id: "03ebe0ae-f630-4414-b37b-afde7de67229"
-        }}
+
+    Or
 
         iex> synchronization_or_id = "03ebe0ae-f630-4414-b37b-afde7de67229"
         "03ebe0ae-f630-4414-b37b-afde7de67229"
+    """,
+    financial_institution_id: """
+    Fetch a financial institution before each example, or use a valid financial institution id
+
+        iex> {:ok, financial_institution_or_id} = Ibanity.PontoConnect.FinancialInstitution.find_public("953934eb-229a-4fd2-8675-07794078cc7d")
+
+    Or
+
+        iex> financial_institution_or_id = "953934eb-229a-4fd2-8675-07794078cc7d"
+        "953934eb-229a-4fd2-8675-07794078cc7d"
+    """,
+    financial_institution_and_account_ids: """
+    Fetch a financial institution and financial institution account before each example, or use a valid financial institution id
+
+        iex> {:ok, financial_institution_or_id} = Ibanity.PontoConnect.FinancialInstitution.find_public(
+        ...>   "953934eb-229a-4fd2-8675-07794078cc7d"
+        ...> )
+        iex> {:ok, financial_institution_account_or_id} = Ibanity.PontoConnect.Sandbox.FinancialInstitutionAccount.find(
+        ...>   token,
+        ...>   %{
+        ...>     financial_institution_id: financial_institution_or_id,
+        ...>     id: "cb0bb5ab-dc3d-4832-b2ff-e6629240732f"
+        ...>   }
+        ...> )
+
+    Or
+
+        iex> financial_institution_or_id = "953934eb-229a-4fd2-8675-07794078cc7d"
+        iex> financial_institution_account_or_id = "cb0bb5ab-dc3d-4832-b2ff-e6629240732f"
+    """,
+    account_and_id_second_arg: """
+    Takes a map with the following keys as second argument:
+    - `:account_id`: `Ibanity.PontoConnect.Account` struct or account ID as a string
+    - `:id`: resource ID as a string
+    """,
+    financial_institution_and_id_second_arg: """
+    Takes a map with the following keys as second argument:
+    - `:financial_institution_id`: `Ibanity.PontoConnect.Sandbox.FinancialInstitution` struct or account ID as a string
+    - `:id`: resource ID as a string
+    """,
+    financial_institution_and_account_second_arg: """
+    Takes a map with the following keys as second argument:
+    - `:financial_institution_id`: `Ibanity.PontoConnect.FinancialInstitution` struct or account ID as a string
+    - `:financial_institution_account_id`: `Ibanity.PontoConnect.Sandbox.FinancialInstitutionAccount` struct or account ID as a string
+    - `:id`: resource ID as a string
     """
   }
 
@@ -208,15 +244,12 @@ defmodule Ibanity.PontoConnect do
   def common_docs!(key), do: Map.fetch!(@common_docs, key)
 
   @doc false
-  def format_account_ids(ids), do: format_ids(ids, [:account_id, :id])
-
-  defp format_ids(ids, []), do: ids
-
-  defp format_ids(ids, [key | rest]) do
-    case Map.fetch!(ids, key) do
-      %{id: id} -> Map.put(ids, key, id)
-      _ -> ids
+  def format_ids(ids) do
+    for {key, val} <- ids, val != nil do
+      case val do
+        %{id: id} -> {key, id}
+        _ -> {key, val}
+      end
     end
-    |> format_ids(rest)
   end
 end
