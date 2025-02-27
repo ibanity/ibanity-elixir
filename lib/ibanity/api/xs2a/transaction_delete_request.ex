0664b5136ae76e7c6c3c94f34b53e4af750d3f78
Fabian Zitter <fzitter@isabelgroup.eu>
2025-02-27 15:05:06 +0100

    Consolidate duplicate function docs

--- lib/ibanity/api/xs2a/transaction_delete_request.ex
+++ lib/ibanity/api/xs2a/transaction_delete_request.ex
@@ -34,16 +34,37 @@ defmodule Ibanity.Xs2a.TransactionDeleteRequest do
   end
 
   @doc """
-  [Creates a new transaction delete request] for financial institution and account(https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-account).
+  Creates a new transaction delete request for:
+  - [financial institution and account](https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-account)
+  - [application](https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-application)
+  - [customer](https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-customer)
 
   Returns `{:ok, transaction_delete_request}` if sucessful, `{:error, reason}` otherwise.
 
   ## Example
 
-      iex> "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9..."
-      ...> |> Request.customer_access_token
+  Transaction delete request for financial institution and account:
+
+      iex> token
+      ...> |> Request.customer_access_token()
       ...> |> Request.attribute("before_date", "2022-01-01T00:00:00.000Z")
       ...> |> Request.ids([financial_institution_id: "b2c76f6b-ab34-4843-8ef3-84300ef98a09", account_id: "616a75d4-0262-4edc-b168-200c9773d8f8"])
+      ...> |> TransactionDeleteRequest.create()
+      {:ok, %TransactionDeleteRequest{id: "9c57df52-bd8e-42d2-b15a-a664741e3ed2"}}
+
+  Transaction delete request for application:
+
+      iex> [before_date: "2022-01-01T00:00:00.000Z"]
+      ...> |> Request.attributes()
+      ...> |> TransactionDeleteRequest.create()
+      {:ok, %TransactionDeleteRequest{id: "9c57df52-bd8e-42d2-b15a-a664741e3ed2"}}
+
+
+  Transaction delete request for customer:
+
+      iex> "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9..."
+      ...> |> Request.customer_access_token()
+      ...> |> Request.attribute(before_date, "2022-01-01T00:00:00.000Z")
       ...> |> TransactionDeleteRequest.create
       {:ok, %TransactionDeleteRequest{id: "9c57df52-bd8e-42d2-b15a-a664741e3ed2"}}
   """
@@ -65,37 +86,12 @@ defmodule Ibanity.Xs2a.TransactionDeleteRequest do
     )
   end
 
-  @doc """
-  [Creates a new transaction delete request] for application(https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-application).
-
-  Returns `{:ok, transaction_delete_request}` if sucessful, `{:error, reason}` otherwise.
-
-  ## Example
-
-      iex> [before_date: "2022-01-01T00:00:00.000Z"]
-      ...> |> Request.attributes
-      ...> |> TransactionDeleteRequest.create
-      {:ok, %TransactionDeleteRequest{id: "9c57df52-bd8e-42d2-b15a-a664741e3ed2"}}
-  """
   def create(%Request{customer_access_token: nil} = request) do
     request
     |> Request.resource_type(@resource_type)
     |> Client.execute(:post, ~w(xs2a transactionDeleteRequests))
   end
 
-  @doc """
-  [Creates a new transaction delete request] for customer(https://documentation.ibanity.com/xs2a/api#create-transaction-delete-request-for-customer).
-
-  Returns `{:ok, transaction_delete_request}` if sucessful, `{:error, reason}` otherwise.
-
-  ## Example
-
-      iex> "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9..."
-      ...> |> Request.customer_access_token
-      ...> |> Request.attribute(before_date, "2022-01-01T00:00:00.000Z")
-      ...> |> TransactionDeleteRequest.create
-      {:ok, %TransactionDeleteRequest{id: "9c57df52-bd8e-42d2-b15a-a664741e3ed2"}}
-  """
   def create(%Request{} = request) do
     request
     |> Request.resource_type(@resource_type)
